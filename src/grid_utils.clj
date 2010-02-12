1ec02a9e5c1af6227141ea040ca8e82ebcd9d1c8
Brian Pattison <brian.pattison@gmail.com>
2010-02-11 19:03:31 -0500

    add copyright

--- src/grid_utils.clj
+++ src/grid_utils.clj
@@ -1,3 +1,11 @@
+;   Copyright (c) Brian Pattison. All rights reserved.
+;   The use and distribution terms for this software are covered by the
+;   Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;   which can be found in the file epl-v10.html at the root of this distribution.
+;   By using this software in any fashion, you are agreeing to be bound by
+;   the terms of this license.
+;   You must not remove this notice, or any other, from this software.
+
 (ns grid-utils
   (:require [clojure.contrib.math :as math]))
   
@@ -11,8 +19,8 @@
           l (last r)]
       (let [[_ y e] l, e (+ e ady)]
         (if (>= (* e 2) adx)
-          (conj r [ x (+ y-dir y) (- e adx) ])
-          (conj r [ x y e ]))))))
+          (conj r [x (+ y-dir y) (- e adx)])
+          (conj r [x y e]))))))
           
 ; y major line
 (defn- y-major-line [y x0 y0 adx ady x-dir y-dir]
@@ -22,20 +30,21 @@
           l (last r)]
       (let [[x _ e] l, e (+ e adx)]
         (if (>= (* e 2) ady )
-          (conj r [ (+ x-dir x) y (- e ady) ])
-          (conj r [ x y e ]))))))
-
+          (conj r [(+ x-dir x) y (- e ady)])
+          (conj r [x y e]))))))
           
 (defn get-line [x0 y0 x1 y1]
   (let [dx  (- x1 x0)
         dy  (- y1 y0)
-        m   (if (= dx 0)
+        m   (if (= dx 0)  ; check for divid by zero
               (* dy java.lang.Double/POSITIVE_INFINITY)
               (/ dy dx))
         adx (math/abs dx)
         ady (math/abs dy)
         am  (math/abs m)]
+        
 ;   (println "[" x0 "," y0 "] [" x1 "," y1 "] m=" m)
+
     (cond
    
       ; q0 
@@ -70,12 +79,12 @@
       (and (< y0 y1) (< 1 m))
         (y-major-line y1 x0 y0 adx ady +1 -1)
         
-      ; no slope, not a line
-      ; happens when x0==x1 and y0==y1  
+      ; no slope, not a line just a point
+      ; happens when x0 == x1 and y0 == y1  
       true [])))
  
  (defn- test-get-line []
    (doseq [x (range -25 25 5) y (range -25 25 5)]
      (println (interpose "\n" (get-line 0 0 x y)))))
 
-;(test-get-line)     
+;(test-get-line)
